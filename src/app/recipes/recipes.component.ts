75057c57ae9a87d4dfff42e0365af5aa03e582ef
Maxwell  Kwarteng <maxwellkwarteng7@gmail.com>
2024-10-16 15:18:22 +0000

    another commit

---
+++ src/app/recipes/recipes.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { RecipeServiceService } from './recipe-service.service';
+import { Recipe } from '../models/interface';
+import { CommonModule } from '@angular/common';
+import {
+  IonHeader, 
+  IonButton,
+  IonInput,
+  IonLabel,
+  IonItem,
+  IonList , 
+  IonToolbar,
+  IonTitle,
+  IonAvatar , 
+  IonContent, 
+  IonImg
+} from '@ionic/angular/standalone'
+
+@Component({
+  selector: 'app-recipes',
+  standalone : true ,
+  templateUrl: './recipes.component.html',
+  styleUrls: ['./recipes.component.scss'],
+  imports : [CommonModule , IonHeader , IonButton , IonContent , IonList , IonItem , IonToolbar , IonTitle , IonImg, IonAvatar], 
+})
+export class RecipesComponent implements OnInit {
+  recipe: Recipe[] = []; 
+
+  constructor(private recipeService: RecipeServiceService) { }
+
+
+  ngOnInit(): void {
+    this.recipe = this.recipeService.getAllRecipes();
+    console.log(this.recipe); 
+  }
+
+
+
+}
